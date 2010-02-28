e8377c6295b316638092e702209b9a0727eab111
djtfmartin <djtfmartin@gmail.com>
2010-02-28 10:24:30 +0000

    vocab test
    git-svn-id: http://ala-bie.googlecode.com/svn/trunk/bie-hbase@515 de2a7fd8-b20c-11de-9ad3-fd5786690477

--- src/test/java/org/ala/vocabulary/VocabularyTest.java
+++ src/test/java/org/ala/vocabulary/VocabularyTest.java
@@ -1,11 +1,31 @@
+/***************************************************************************
+ * Copyright (C) 2010 Atlas of Living Australia
+ * All Rights Reserved.
+ *
+ * The contents of this file are subject to the Mozilla Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ ***************************************************************************/
 package org.ala.vocabulary;
 
 import junit.framework.TestCase;
 
 import org.ala.model.ConservationStatus;
+import org.ala.model.PestStatus;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+/**
+ * JUnit tests for Vocabulary API
+ *
+ * @author Dave Martin (David.Martin@csiro.au)
+ */
 public class VocabularyTest extends TestCase {
 
 	public void testConservationStatusLookup(){
@@ -17,7 +37,9 @@ public class VocabularyTest extends TestCase {
 	}
 
 	public void testPestStatusLookup(){
-		
-		
+		ApplicationContext context = new ClassPathXmlApplicationContext("classpath:spring.xml");
+		Vocabulary vocabulary = (Vocabulary) context.getBean("vocabulary");
+		PestStatus ps = vocabulary.getPestStatusFor(1023, "Exotic (absent from Australia)");
+		assertEquals("invasive", ps.getStatus());
 	}
 }
