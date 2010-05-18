841b99fe1275016a93497121ade5dfb580d0cec2
djtfmartin <djtfmartin@gmail.com>
2010-05-18 04:53:14 +0000

    added volume and year
    git-svn-id: http://ala-bie.googlecode.com/svn/trunk/bie-hbase@1018 de2a7fd8-b20c-11de-9ad3-fd5786690477

--- scripts/bhl/export.sql
+++ scripts/bhl/export.sql
@@ -1,7 +1,9 @@
 -- This is ran on the BHL database using the SQLServer console tools
 -- with a report to File option (see the context menu - right click - in the SQL query pane)
 
-select t.TitleID, t.FullTitle, t.ShortTitle, i.ItemID, p.PageID, pn.NameConfirmed, pn.NameFound
+-- Under "Text" on LHS, select Output format: "Tab delimited"
+
+select t.TitleID, t.FullTitle, t.ShortTitle, i.ItemID, i.Volume, i.Year, p.PageID, pn.NameConfirmed, pn.NameFound
 from BHL.dbo.Item i
 inner join BHL.dbo.Title t ON i.PrimaryTitleID = t.TitleID
 inner join BHL.dbo.Page p ON p.ItemID = i.ItemID
