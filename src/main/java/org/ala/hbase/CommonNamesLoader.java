5719aa4c2f3f4bac0b6201536dd9e1590b188332
djtfmartin <djtfmartin@gmail.com>
2010-05-30 08:37:33 +0000

    loader for common names
    git-svn-id: http://ala-bie.googlecode.com/svn/trunk/bie-hbase@1068 de2a7fd8-b20c-11de-9ad3-fd5786690477

---
+++ src/main/java/org/ala/hbase/CommonNamesLoader.java
@@ -0,0 +1,95 @@
+/***************************************************************************
+ * Copyright (C) 2010 Atlas of Living Australia
+ * All Rights Reserved.
+ *
+ * The contents of this file are subject to the Mozilla Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ ***************************************************************************/
+package org.ala.hbase;
+
+import javax.inject.Inject;
+
+import org.ala.dao.TaxonConceptDao;
+import org.ala.model.CommonName;
+import org.ala.util.SpringUtils;
+import org.ala.util.TabReader;
+import org.apache.log4j.Logger;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Component;
+
+/**
+ * Simple class to load in the common names as supplied by the ANBG.
+ *
+ * @author Dave Martin (David.Martin@csiro.au)
+ */
+@Component("commonNamesLoader")
+public class CommonNamesLoader {
+
+	protected Logger logger = Logger.getLogger(CommonNamesLoader.class);
+	
+	private static final String AFD_COMMON_NAMES = "/data/bie-staging/anbg/AFD-common-names.csv";
+	private static final String APNI_COMMON_NAMES = "/data/bie-staging/anbg/APNI-common-names.csv";
+	
+	@Inject
+	protected TaxonConceptDao taxonConceptDao;
+	
+	public static void main(String[] args) throws Exception {
+		ApplicationContext context = SpringUtils.getContext();
+		CommonNamesLoader l = context.getBean(CommonNamesLoader.class);
+		l.load();
+	}
+	
+	/**
+	 * @throws Exception
+	 */
+	private void load() throws Exception {
+		// For testing
+		loadCommonNames(AFD_COMMON_NAMES);
+	}
+
+	/**
+	 * @param regionDatFile
+	 * @throws Exception
+	 */
+	private void loadCommonNames(String dataFile) throws Exception {
+		logger.info("Starting to load region occurrences from " + dataFile);
+		
+    	long start = System.currentTimeMillis();
+    	
+    	// add the taxon concept regions
+    	TabReader tr = new TabReader(dataFile, true, ',');
+    	String[] values = null;
+    	int namesAdded = 0;
+		while ((values = tr.readNext()) != null) {
+    		if (values.length > 5) {
+    			String guid = values[1];
+    			String commonNameString = values[2];
+    			String taxonConceptGuid = values[5];
+    			
+    			CommonName commonName = new CommonName();
+    			commonName.setGuid(guid);
+    			commonName.setNameString(commonNameString);
+    			boolean success = taxonConceptDao.addCommonName(taxonConceptGuid, commonName);
+    			if(success) namesAdded++;
+    		}
+		}
+		
+    	tr.close();
+		long finish = System.currentTimeMillis();
+		logger.info(namesAdded+" common names added to taxa. Time taken "+(((finish-start)/1000)/60)+" minutes, "+(((finish-start)/1000) % 60)+" seconds.");
+	}
+
+	/**
+	 * @param taxonConceptDao the taxonConceptDao to set
+	 */
+	public void setTaxonConceptDao(TaxonConceptDao taxonConceptDao) {
+		this.taxonConceptDao = taxonConceptDao;
+	}
+}
