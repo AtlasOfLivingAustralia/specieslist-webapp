bc653bb09794e1f9bfc94946ed1a669325da1e07
djtfmartin <djtfmartin@gmail.com>
2010-07-16 06:00:48 +0000

    counts for occurrence records
    git-svn-id: http://ala-bie.googlecode.com/svn/trunk/bie-webapp@1269 de2a7fd8-b20c-11de-9ad3-fd5786690477

--- src/main/java/org/ala/web/InfosourceController.java
+++ src/main/java/org/ala/web/InfosourceController.java
@@ -1,3 +1,17 @@
+/**************************************************************************
+ *  Copyright (C) 2010 Atlas of Living Australia
+ *  All Rights Reserved.
+ *
+ *  The contents of this file are subject to the Mozilla Public
+ *  License Version 1.1 (the "License"); you may not use this file
+ *  except in compliance with the License. You may obtain a copy of
+ *  the License at http://www.mozilla.org/MPL/
+ *
+ *  Software distributed under the License is distributed on an "AS
+ *  IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ *  implied. See the License for the specific language governing
+ *  rights and limitations under the License.
+ ***************************************************************************/
 package org.ala.web;
 
 import java.io.IOException;
@@ -10,9 +24,6 @@ import java.util.Map;
 import javax.inject.Inject;
 import javax.servlet.http.HttpServletResponse;
 
-import net.sf.json.JSONArray;
-import net.sf.json.JSONObject;
-
 import org.ala.dao.FulltextSearchDao;
 import org.ala.dao.InfoSourceDAO;
 import org.ala.dao.VocabularyDAO;
@@ -22,7 +33,6 @@ import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.methods.GetMethod;
 import org.apache.log4j.Logger;
 import org.codehaus.jackson.JsonNode;
-import org.codehaus.jackson.JsonProcessingException;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -53,7 +63,7 @@ public class InfosourceController {
 	private final String DATASET_LIST = "species/datasetList";
 	/** Name of view for list of vocabularies */
 	private final String VOCABULARIES_LIST = "species/vocabularies";
-	
+	/** added data provider counts */
 	protected String dataProviderCounts = "http://biocache.ala.org.au/data_providers/counts.json";
 	
 	/**
@@ -75,8 +85,10 @@ public class InfosourceController {
 			}
 		}
 		
+		//retrieve the occurrence infosources
 		List<InfoSource> occurrenceInfoSources = retrieveOccurrenceInfoSources();
-        model.addAttribute("occurrenceInfoSources", occurrenceInfoSources);
+        
+		model.addAttribute("occurrenceInfoSources", occurrenceInfoSources);
 		model.addAttribute("infoSources", infoSources);
 		model.addAttribute("infoSourceIDWithVocabulariesMapList", infoSourceIDWithVocabulariesMapList);
 		Map<String, Long> countsMap = searchDao.getAllDatasetCounts();
@@ -111,6 +123,7 @@ public class InfosourceController {
 					i.setId(Integer.parseInt(id));
 					i.setName(name);
 					i.setDocumentCount(count);
+					occurrenceInfoSources.add(i);
 				}
 			}
 		} catch (Exception e) {
