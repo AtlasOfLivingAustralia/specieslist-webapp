f7361ab720b7b10421d7619e64029320d06dcdd7
djtfmartin <djtfmartin@gmail.com>
2010-08-05 04:47:13 +0000

    simple ranking
    git-svn-id: http://ala-bie.googlecode.com/svn/trunk/bie-hbase@1454 de2a7fd8-b20c-11de-9ad3-fd5786690477

---
+++ src/test/java/org/ala/dao/RankingDaoTest.java
@@ -0,0 +1,33 @@
+package org.ala.dao;
+
+import org.ala.util.SpringUtils;
+
+import junit.framework.TestCase;
+
+public class RankingDaoTest extends TestCase {
+
+	public void testRanking() throws Exception {
+		
+		RankingDao rankingDao = SpringUtils.getContext().getBean(RankingDao.class);
+		
+		boolean created = rankingDao.rankImageForTaxon(
+				"12.12.12.12", 
+				"davejmartin", 
+				"urn:lsid:biodiversity.org.au:apni.taxon:296831", 
+				"Acacia dealbata", 
+				"http://upload.wikimedia.org/wikipedia/commons/d/df/Acacia_dealbata_AF.jpg", 
+				1036, 
+				true);
+		
+		created = rankingDao.rankImageForTaxon(
+				"12.12.12.12", 
+				"davejmartin", 
+				"urn:lsid:biodiversity.org.au:apni.taxon:296831", 
+				"Acacia dealbata", 
+				"http://upload.wikimedia.org/wikipedia/commons/8/82/Acacia_dealbata-1.jpg",
+				1036, 
+				false);
+		
+		System.out.println("Loaded:"+created);
+	}
+}
