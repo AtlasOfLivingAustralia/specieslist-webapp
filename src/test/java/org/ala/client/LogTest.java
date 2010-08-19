228a24348d314e098da6ae26606062e91761c227
dcwmk282@gmail.com <dcwmk282@gmail.com>
2010-08-19 07:48:45 +0000

    git-svn-id: http://ala-bie.googlecode.com/svn/trunk/ala-logger@1634 de2a7fd8-b20c-11de-9ad3-fd5786690477

--- src/test/java/org/ala/client/LogTest.java
+++ src/test/java/org/ala/client/LogTest.java
@@ -10,9 +10,24 @@ import org.apache.log4j.Logger;
 
 public class LogTest {
 	private static Logger logger = Logger.getLogger(LogTest.class);
+
 	/**
-	 * @param args
-	 */
+	 * URL: http://152.83.198.112:8080/ala-logger/service/logger/
+	 * METHOD: "POST"
+	 * 
+	 * Expected json input format:
+	 * {
+   	 * "eventTypeId": 12345,
+     * "comment": "For doing some research with..",
+     * "userEmail" : "David.Martin@csiro.au",
+     * "recordCounts" : {
+     * "dp123": 32,
+     * "dr143": 22,
+     *  "ins322": 55
+   	 * }
+	 * }
+	 * 
+	*/
 	public static void main(String[] args) {
     	StringBuffer sb = new StringBuffer();
     	
@@ -31,13 +46,13 @@ public class LogTest {
     	logger.log(RestLevel.REMOTE, sb.toString());
     	        	
     	Map<String, Integer> recordCounts = new Hashtable<String, Integer>();
+    	// entityUid, record_count
     	recordCounts.put("dp123", 32);
     	recordCounts.put("dr143", 22);
     	recordCounts.put("ins322", 55);
     	LogEventVO vo = new LogEventVO(123, "waiman.mok@csiro.au", "For doing some research with", recordCounts);
     	logger.log(RestLevel.REMOTE, vo);
-    	
-    	
+    	    	
     	LogManager.shutdown();
 
     	return;
