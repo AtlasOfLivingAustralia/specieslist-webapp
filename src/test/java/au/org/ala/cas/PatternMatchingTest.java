0a713aa26115b408a5c0ba9b53dd6e3a508fbaa3
peter.flemming@csiro.au <peter.flemming@csiro.au>
2010-09-01 07:37:16 +0000

    Updated test for contextPath.
    git-svn-id: http://ala-bie.googlecode.com/svn/trunk/ala-cas-client@1705 de2a7fd8-b20c-11de-9ad3-fd5786690477

--- src/test/java/au/org/ala/cas/PatternMatchingTest.java
+++ src/test/java/au/org/ala/cas/PatternMatchingTest.java
@@ -9,22 +9,23 @@ import au.org.ala.cas.util.PatternMatchingUtils;
 public class PatternMatchingTest extends TestCase {
 
 	public void testMatches() {
-		String uriPattern = "^$, /, /biocache-webapp/, /biocache-webapp/occurrences/\\d+";
-		List<Pattern> patterns = PatternMatchingUtils.getPatternList(uriPattern);
-		assertTrue(PatternMatchingUtils.matches("", patterns));
-		assertTrue(PatternMatchingUtils.matches("/", patterns));
+		String contextPath = "/biocache-webapp";
+		String uriPattern = "/, /occurrences/\\d+";
+		List<Pattern> patterns = PatternMatchingUtils.getPatternList(contextPath, uriPattern);
+		assertFalse(PatternMatchingUtils.matches("", patterns));
+		assertFalse(PatternMatchingUtils.matches("/", patterns));
 		assertTrue(PatternMatchingUtils.matches("/biocache-webapp/", patterns));
 		assertTrue(PatternMatchingUtils.matches("/biocache-webapp/occurrences/35661424", patterns));
 		assertFalse(PatternMatchingUtils.matches("/favicon.ico", patterns));
 
-		List<Pattern> noPatterns = PatternMatchingUtils.getPatternList(null);
+		List<Pattern> noPatterns = PatternMatchingUtils.getPatternList(null, null);
 		assertFalse(PatternMatchingUtils.matches("", noPatterns));
 
-		List<Pattern> emptyPatterns = PatternMatchingUtils.getPatternList("");
+		List<Pattern> emptyPatterns = PatternMatchingUtils.getPatternList("", "");
 		assertFalse(PatternMatchingUtils.matches("", emptyPatterns));
 		
 		try {
-			List<Pattern> badPatterns = PatternMatchingUtils.getPatternList("\\k");
+			List<Pattern> badPatterns = PatternMatchingUtils.getPatternList("", "\\k");
 			PatternMatchingUtils.matches("", badPatterns);
 			fail("Bad pattern exception not thrown");
 		} catch (Exception e) {
