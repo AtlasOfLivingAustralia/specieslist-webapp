8b61845038b99af1fe18958bef68b2078385b6b8
peter.flemming@csiro.au <peter.flemming@csiro.au>
2010-09-01 07:31:07 +0000

    Added contextPath parameter handling and copyright notice.
    git-svn-id: http://ala-bie.googlecode.com/svn/trunk/ala-cas-client@1704 de2a7fd8-b20c-11de-9ad3-fd5786690477

--- src/main/java/au/org/ala/cas/util/PatternMatchingUtils.java
+++ src/main/java/au/org/ala/cas/util/PatternMatchingUtils.java
@@ -1,3 +1,17 @@
+/***************************************************************************
+ * Copyright (C) 2010 Atlas of Living Australia
+ * All Rights Reserved.
+ *
+ * The contents of this file are subject to the Mozilla Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ ***************************************************************************/
 package au.org.ala.cas.util;
 
 import java.util.ArrayList;
@@ -11,12 +25,12 @@ public class PatternMatchingUtils {
 
 	private final static Logger logger = Logger.getLogger(PatternMatchingUtils.class);
 
-	public static List<Pattern> getPatternList(String uriPatterns) {
+	public static List<Pattern> getPatternList(String context, String uriPatterns) {
 		List<Pattern> patternList = new ArrayList<Pattern>();
 
 		if (uriPatterns != null && !uriPatterns.equals("")) {
 			for (String pattern : uriPatterns.split(",")) {
-				patternList.add(Pattern.compile(pattern.trim(), Pattern.CASE_INSENSITIVE));
+				patternList.add(Pattern.compile(context + pattern.trim(), Pattern.CASE_INSENSITIVE));
 			}
 		}
 		
