1897c1ad721cbf77bbcc3fc35b31274e06f3df7c
dcwmk282@gmail.com <dcwmk282@gmail.com>
2010-10-05 05:52:48 +0000

    git-svn-id: http://ala-bie.googlecode.com/svn/trunk/bie-hbase@1939 de2a7fd8-b20c-11de-9ad3-fd5786690477

--- src/main/java/org/ala/hbase/IdentifyLifeLoader.java
+++ src/main/java/org/ala/hbase/IdentifyLifeLoader.java
@@ -73,9 +73,9 @@ public class IdentifyLifeLoader {
 			System.out.println("load process finished.....");
 		} catch (Exception e) {			
 			e.printStackTrace();
-			System.exit(0);
+			System.exit(1);
 		}
-		System.exit(1);
+		System.exit(0);
 	}
 	
 	/**
