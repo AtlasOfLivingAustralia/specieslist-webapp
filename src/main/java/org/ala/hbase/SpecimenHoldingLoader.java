0f083518eaabae3f52def812c53e8cc3ebf40524
dcwmk282@gmail.com <dcwmk282@gmail.com>
2010-10-05 04:37:01 +0000

    init version
    git-svn-id: http://ala-bie.googlecode.com/svn/trunk/bie-hbase@1934 de2a7fd8-b20c-11de-9ad3-fd5786690477

---
+++ src/main/java/org/ala/hbase/SpecimenHoldingLoader.java
@@ -0,0 +1,193 @@
+/***************************************************************************
+ * Copyright (C) 2010 Atlas of Living Australia
+ * All Rights Reserved.
+ *
+ * The contents of this file are subject to the Mozilla Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ ***************************************************************************/
+package org.ala.hbase;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.inject.Inject;
+import org.ala.dao.InfoSourceDAO;
+import org.ala.dao.TaxonConceptDao;
+import org.ala.model.InfoSource;
+import org.ala.model.SpecimenHolding;
+import org.ala.util.SpringUtils;
+import org.apache.log4j.Logger;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Component;
+
+import au.com.bytecode.opencsv.CSVReader;
+
+/**
+ * This class loads data from Botanical Gardens data file (data.csv) into the Cassandra
+ * 
+ * @author MOK011
+ *
+ */
+
+@Component("specimenHoldingLoader")
+public class SpecimenHoldingLoader {	
+	protected static Logger logger  = Logger.getLogger(SpecimenHoldingLoader.class);	
+	private static final String INPUT_FILE_NAME = "/data/bie-staging/botanticalGardens/data.csv";
+	@Inject
+	protected InfoSourceDAO infoSourceDao;	
+	@Inject
+	protected TaxonConceptDao taxonConceptDao;
+	
+    public static enum BOTANTICAL_GARDENS_IDX {URL, INSTITUTION, SITE_NAME, FAMILY, GENUS, HYBRID_Q, SPECIES,
+		SCIENCETIFIC_NAME, INFRASPECIFIC_Q, INFRASPECIFIC_NAME, CULTIVAR, COMMON_NAME, NOTES, COUNT}    
+	
+
+	public static void main(String[] args) {
+		System.out.println("Starting SpecimenHoldingLoader process.....");
+		ApplicationContext context = SpringUtils.getContext();
+		SpecimenHoldingLoader l = context.getBean(SpecimenHoldingLoader.class);
+		try {
+			System.out.println("Starting load process.....");
+			l.load();
+			System.out.println("load process finished.....");
+		} catch (Exception e) {			
+			e.printStackTrace();
+			System.exit(0);
+		}
+		System.exit(1);
+	}
+	
+	/**
+	 * data load process
+	 */
+	private void load(){
+		String[] nextLine = null;
+		CSVReader reader = null;
+
+		try{
+			reader = new CSVReader(new FileReader(INPUT_FILE_NAME));
+			//ignore first header line
+			nextLine = reader.readNext();
+			//first data line
+			nextLine = reader.readNext();
+			while (nextLine != null) {
+				List<SpecimenHolding> list = new ArrayList<SpecimenHolding> ();
+				SpecimenHolding sh = toSpecimenHolding(nextLine);
+				if(sh == null){
+					System.out.println("*** SCIENCETIFIC_NAME: " + nextLine[BOTANTICAL_GARDENS_IDX.SCIENCETIFIC_NAME.ordinal()] + ", guid: ");	
+				}
+				else{
+					System.out.println("*** SCIENCETIFIC_NAME: " + nextLine[BOTANTICAL_GARDENS_IDX.SCIENCETIFIC_NAME.ordinal()] + ", guid: " + sh.getIdentifier());
+				}
+				nextLine = reader.readNext();
+				// have guid & infosource ....
+				if(sh != null){
+					list.add(sh);
+					// more than one idKey for same guid, add into same list (same cassandra column)
+					while (nextLine != null) {
+						SpecimenHolding nextKey = toSpecimenHolding(nextLine);																		
+						if(nextKey != null && sh.getIdentifier().equals(nextKey.getIdentifier())){
+							System.out.println("*** SCIENCETIFIC_NAME: " + nextLine[BOTANTICAL_GARDENS_IDX.SCIENCETIFIC_NAME.ordinal()] + ", guid: " + nextKey.getIdentifier());
+							list.add(nextKey);
+							nextLine = reader.readNext();
+						}
+						else{
+							break;
+						}
+					}					
+				}
+				if(!list.isEmpty() && sh.getIdentifier() != null && sh.getIdentifier().length() > 0){
+					try {
+						taxonConceptDao.addSpecimenHoldings(sh.getIdentifier(), list);
+					} catch (Exception e) {
+						logger.error(e);
+						e.printStackTrace();
+					}
+				}
+			}
+		}
+		catch (IOException e) {
+			logger.error(e);
+			e.printStackTrace();
+		}
+		finally{
+			if(reader != null){
+				try {
+					reader.close();
+				} catch (IOException e) {
+					logger.error(e);
+				}
+			}
+		}
+	}
+
+	/**
+	 * populate data into SpecimenHolding.
+	 * 
+	 * @param idLifeData csv data line
+	 * @return
+	 */
+	private SpecimenHolding toSpecimenHolding(String[] data){
+		SpecimenHolding o = null;
+		String tmp = null;
+		
+		// get guid & infosource
+		String guid = taxonConceptDao.findLsidByName(data[BOTANTICAL_GARDENS_IDX.SCIENCETIFIC_NAME.ordinal()].trim());
+		InfoSource infosource = infoSourceDao.getByUri(data[BOTANTICAL_GARDENS_IDX.URL.ordinal()].trim());
+		logger.debug("guid: " + guid + ", infosourceId: " + infosource);
+		if(guid != null && guid.length() > 0 && infosource != null){			
+			o = new SpecimenHolding();			
+			o.setInfoSourceId("" + infosource.getId());
+			o.setInfoSourceName(infosource.getName());
+			o.setInfoSourceURL(data[BOTANTICAL_GARDENS_IDX.URL.ordinal()].trim());
+			o.setIdentifier(guid);
+			
+			//some data in csv file have less column, copy it into fix size array.
+			String[] copy = new String[BOTANTICAL_GARDENS_IDX.values().length];
+			for(int j = data.length; j < copy.length; j++){
+				copy[j] = "";
+			}
+			//copy data
+			for(int i = 0; i < data.length; i++){
+				copy[i] = data[i];
+			}
+			
+			o.setUrl(copy[BOTANTICAL_GARDENS_IDX.URL.ordinal()].trim());
+			o.setInstitutionName(copy[BOTANTICAL_GARDENS_IDX.INSTITUTION.ordinal()].trim());
+			o.setSiteName(copy[BOTANTICAL_GARDENS_IDX.SITE_NAME.ordinal()].trim());
+			o.setFamily(copy[BOTANTICAL_GARDENS_IDX.FAMILY.ordinal()].trim());
+			o.setGenus(copy[BOTANTICAL_GARDENS_IDX.GENUS.ordinal()].trim());
+			o.setHybirdQualifier(copy[BOTANTICAL_GARDENS_IDX.HYBRID_Q.ordinal()].trim());
+			o.setSpecies(copy[BOTANTICAL_GARDENS_IDX.SPECIES.ordinal()].trim());
+			o.setScientificName(copy[BOTANTICAL_GARDENS_IDX.SCIENCETIFIC_NAME.ordinal()].trim());
+			o.setInfraspecificQualifier(copy[BOTANTICAL_GARDENS_IDX.INFRASPECIFIC_Q.ordinal()].trim());
+			o.setInfraspecificName(copy[BOTANTICAL_GARDENS_IDX.INFRASPECIFIC_NAME.ordinal()].trim());
+			o.setCultivar(copy[BOTANTICAL_GARDENS_IDX.CULTIVAR.ordinal()].trim());
+			o.setCommonName(copy[BOTANTICAL_GARDENS_IDX.COMMON_NAME.ordinal()].trim());
+			o.setNotes(copy[BOTANTICAL_GARDENS_IDX.NOTES.ordinal()].trim());
+			
+			tmp = copy[BOTANTICAL_GARDENS_IDX.COUNT.ordinal()].trim();
+			if(tmp != null && tmp.length() > 0){
+				o.setCount(Integer.parseInt(tmp));
+			}
+		}
+		else{
+			if(infosource == null){
+				logger.warn("Unable to find infosource : " + data[BOTANTICAL_GARDENS_IDX.URL.ordinal()].trim());
+			}
+			else{
+				logger.warn("Unable to find LSID for '" + data[BOTANTICAL_GARDENS_IDX.SCIENCETIFIC_NAME.ordinal()].trim() + "'");
+			}
+		}
+		return o;
+	}
+}
+
---
+++ src/main/java/org/ala/model/SpecimenHolding.java
@@ -0,0 +1,138 @@
+package org.ala.model;
+
+public class SpecimenHolding extends AttributableObject implements Comparable<SpecimenHolding> {
+	private String url;
+	private String institutionName;
+	private String siteName;
+	private String family;
+	private String genus;
+	private String hybirdQualifier;
+	private String species;
+	private String scientificName;
+	private String infraspecificQualifier;
+	private String infraspecificName;
+	private String cultivar;
+	private String commonName;
+	private String notes;
+	private int count;
+	
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getInstitutionName() {
+		return institutionName;
+	}
+
+	public void setInstitutionName(String institutionName) {
+		this.institutionName = institutionName;
+	}
+
+	public String getSiteName() {
+		return siteName;
+	}
+
+	public void setSiteName(String siteName) {
+		this.siteName = siteName;
+	}
+
+	public String getFamily() {
+		return family;
+	}
+
+	public void setFamily(String family) {
+		this.family = family;
+	}
+
+	public String getGenus() {
+		return genus;
+	}
+
+	public void setGenus(String genus) {
+		this.genus = genus;
+	}
+
+	public String getHybirdQualifier() {
+		return hybirdQualifier;
+	}
+
+	public void setHybirdQualifier(String hybirdQualifier) {
+		this.hybirdQualifier = hybirdQualifier;
+	}
+
+	public String getSpecies() {
+		return species;
+	}
+
+	public void setSpecies(String species) {
+		this.species = species;
+	}
+
+	public String getScientificName() {
+		return scientificName;
+	}
+
+	public void setScientificName(String scientificName) {
+		this.scientificName = scientificName;
+	}
+
+	public String getInfraspecificQualifier() {
+		return infraspecificQualifier;
+	}
+
+	public void setInfraspecificQualifier(String infraspecificQualifier) {
+		this.infraspecificQualifier = infraspecificQualifier;
+	}
+
+	public String getInfraspecificName() {
+		return infraspecificName;
+	}
+
+	public void setInfraspecificName(String infraspecificName) {
+		this.infraspecificName = infraspecificName;
+	}
+
+	public String getCultivar() {
+		return cultivar;
+	}
+
+	public void setCultivar(String cultivar) {
+		this.cultivar = cultivar;
+	}
+
+	public String getCommonName() {
+		return commonName;
+	}
+
+	public void setCommonName(String commonName) {
+		this.commonName = commonName;
+	}
+
+	public String getNotes() {
+		return notes;
+	}
+
+	public void setNotes(String notes) {
+		this.notes = notes;
+	}
+
+	public int getCount() {
+		return count;
+	}
+
+	public void setCount(int count) {
+		this.count = count;
+	}
+	
+	@Override
+	public int compareTo(SpecimenHolding o) {
+		if (o.getScientificName() != null && this.getScientificName() != null) {
+			return this.getScientificName().compareTo(o.getScientificName());
+		}
+		return -1;
+	}
+}
