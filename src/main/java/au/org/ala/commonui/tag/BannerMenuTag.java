cc1cd6cfff8d064da42ecbbdcf8f74a549d8be3d
djtfmartin <djtfmartin@gmail.com>
2010-10-12 01:09:17 +0000

    added my profile, and removed hardcoding of properties
    git-svn-id: http://ala-bie.googlecode.com/svn/trunk/ala-common-ui@1998 de2a7fd8-b20c-11de-9ad3-fd5786690477

--- src/main/java/au/org/ala/commonui/tag/BannerMenuTag.java
+++ src/main/java/au/org/ala/commonui/tag/BannerMenuTag.java
@@ -35,19 +35,32 @@ public class BannerMenuTag extends TagSupport {
 	
 	private String returnUrlPath = "";
 	
+	protected String defaultCasServer = "https://auth.ala.org.au";
+	protected String defaultCentralServer = "http://test.ala.org.au";
+	
+	
 	/**
 	 * @see javax.servlet.jsp.tagext.TagSupport#doStartTag()
 	 */
 	public int doStartTag() throws JspException {
+
+		HttpServletRequest request = (HttpServletRequest) pageContext.getRequest();
+		Principal principal = request.getUserPrincipal();
+		
+		String casServer = pageContext.getServletContext().getInitParameter("casServerName");
+		if(casServer==null){
+			casServer = defaultCasServer;
+		}
+		
+		String centralServer = pageContext.getServletContext().getInitParameter("centralServer");		
+		if(centralServer==null){
+			centralServer = defaultCentralServer;
+		}
 		
 		String loginLogoutListItem;
 		if (returnUrlPath.equals("")) {
 			loginLogoutListItem = "";
 		} else {
-			HttpServletRequest request = (HttpServletRequest) pageContext.getRequest();
-			Principal principal = request.getUserPrincipal();
-			String casServer = pageContext.getServletContext().getInitParameter("casServerName");
-
 			if (principal == null) {
 				loginLogoutListItem = "<li class='nav-login nav-right'><a href='" + casServer + "/cas/login?service=" + returnUrlPath + "'>Log in</a></li>";
 			} else {
@@ -59,56 +72,57 @@ public class BannerMenuTag extends TagSupport {
 			"<div id='nav'>" +
 				"<!-- WP Menubar 4.7: start menu nav-site, template Superfish, CSS  -->" +
 				"<ul class='sf'>" +
-					"<li class='nav-home'><a href='http://test.ala.org.au/'><span>Home</span></a></li>" +
-					"<li class='nav-explore'><a href='http://test.ala.org.au/explore/'><span>Explore</span></a>" +
+					"<li class='nav-home'><a href='" + centralServer + "/'><span>Home</span></a></li>" +
+					"<li class='nav-explore'><a href='" + centralServer + "/explore/'><span>Explore</span></a>" +
 						"<ul>" +
 							"<li><a href='http://biocache.ala.org.au/explore/your-area'><span>Your Area</span></a></li>" +
-							"<li><a href='http://bie.ala.org.au/regions/'><span>States &amp; Territories</span></a></li>" +
-							"<li><a href='http://test.ala.org.au/explore/species-maps/'><span>Species Maps</span></a></li>" +
+							"<li><a href='http://bie.ala.org.au/regions/'><span>Regions</span></a></li>" +
+							"<li><a href='" + centralServer + "/explore/species-maps/'><span>Species Maps</span></a></li>" +
 							"<li><a href='http://collections.ala.org.au/public/map'><span>Natural History Collections</span></a></li>" +
-							"<li><a href='http://test.ala.org.au/explore/themes/'><span>Themes &amp; Highlights </span></a></li>" +
+							"<li><a href='" + centralServer + "/explore/themes/'><span>Themes &amp; Highlights </span></a></li>" +
 						"</ul>" +
 					"</li>" +
-					"<li class='nav-tools'><a href='http://test.ala.org.au/tools-services/'><span>Tools</span></a>" +
+					"<li class='nav-tools'><a href='" + centralServer + "/tools-services/'><span>Tools</span></a>" +
 						"<ul>" +
-							"<li><a href='http://test.ala.org.au/tools-services/creative-commons-licensing/'><span>Creative Commons licensing</span></a></li>" +
-							"<li><a href='http://test.ala.org.au/tools-services/community-science/'><span>Citizen Science</span></a></li>" +
-							"<li><a href='http://test.ala.org.au/tools-services/identification-tools/'><span>Identification Tools</span></a></li>" +
-							"<li><a href='http://test.ala.org.au/tools-services/for-developers/'><span>For Developers</span></a></li>" +
-							"<li><a href='http://test.ala.org.au/tools-services/sharing-data-through-the-atlas/'><span>Sharing data through the Atlas</span></a></li>" +
-							"<li><a href='http://test.ala.org.au/tools-services/taxonomic-databases-working-group/'><span>TDWG</span></a></li>" +
+							"<li><a href='" + centralServer + "/tools-services/creative-commons-licensing/'><span>Creative Commons licensing</span></a></li>" +
+							"<li><a href='" + centralServer + "/tools-services/community-science/'><span>Citizen Science</span></a></li>" +
+							"<li><a href='" + centralServer + "/tools-services/identification-tools/'><span>Identification Tools</span></a></li>" +
+							"<li><a href='" + centralServer + "/tools-services/for-developers/'><span>For Developers</span></a></li>" +
+							"<li><a href='" + centralServer + "/tools-services/sharing-data-through-the-atlas/'><span>Sharing data through the Atlas</span></a></li>" +
+							"<li><a href='" + centralServer + "/tools-services/taxonomic-databases-working-group/'><span>TDWG</span></a></li>" +
 						"</ul>" +
 					"</li>" +
-					"<li class='nav-contribute'><a href='http://test.ala.org.au/contribute/' title='Contribute - links, images, images, literature, your time'><span>Contribute</span></a>" +
+					"<li class='nav-contribute'><a href='" + centralServer + "/contribute/' title='Contribute - links, images, images, literature, your time'><span>Contribute</span></a>" +
 						"<ul>" +
-							"<li><a href='http://test.ala.org.au/contribute/general-contribute/'><span>General Comments</span></a></li>" +
-							"<li><a href='http://test.ala.org.au/contribute/sighting/'><span>Record Sightings</span></a></li>" +
-							"<li><a href='http://test.ala.org.au/contribute/share-links/'><span>share links, ideas, information</span></a></li>" +
-							"<li><a href='http://test.ala.org.au/contribute/share-images/'><span>Share Photos</span></a></li>" +
-							"<li><a href='http://test.ala.org.au/contribute/share-data/'><span>Upload Data Sets</span></a></li>" +
-							"<li><a href='http://test.ala.org.au/contribute/share-analogue-data/'><span>Non-digital Information</span></a></li>" +
+							"<li><a href='" + centralServer + "/contribute/general-contribute/'><span>General Comments</span></a></li>" +
+							"<li><a href='" + centralServer + "/contribute/sighting/'><span>Record Sightings</span></a></li>" +
+							"<li><a href='" + centralServer + "/contribute/share-links/'><span>share links, ideas, information</span></a></li>" +
+							"<li><a href='" + centralServer + "/contribute/share-images/'><span>Share Photos</span></a></li>" +
+							"<li><a href='" + centralServer + "/contribute/share-data/'><span>Upload Data Sets</span></a></li>" +
+							"<li><a href='" + centralServer + "/contribute/share-analogue-data/'><span>Non-digital Information</span></a></li>" +
 						"</ul>" +
 					"</li>" +
-					"<li class='nav-support'><a href='http://test.ala.org.au/support/'><span>Support</span></a>" +
+					"<li class='nav-support'><a href='" + centralServer + "/support/'><span>Support</span></a>" +
 						"<ul>" +
-							"<li><a href='http://test.ala.org.au/support/get-started/'><span>Get Started</span></a></li>" +
-							"<li><a href='http://test.ala.org.au/support/forum/'><span>Forum</span></a></li>" +
-							"<li><a href='http://test.ala.org.au/support/faq/'><span>Frequently Asked Questions</span></a></li>" +
-							"<li><a href='http://test.ala.org.au/support/how-to/'><span>How To</span></a></li>" +
+							"<li><a href='" + centralServer + "/support/get-started/'><span>Get Started</span></a></li>" +
+							"<li><a href='" + centralServer + "/support/forum/'><span>Forum</span></a></li>" +
+							"<li><a href='" + centralServer + "/support/faq/'><span>Frequently Asked Questions</span></a></li>" +
+							"<li><a href='" + centralServer + "/support/how-to/'><span>How To</span></a></li>" +
 						"</ul>" +
 					"</li>" +
-					"<li class='nav-contact'><a href='http://test.ala.org.au/contact-us/'><span>Contact Us</span></a></li>" +
-					"<li class='nav-about'><a href='http://test.ala.org.au/about/'><span>About the Atlas</span></a>" +
+					"<li class='nav-contact'><a href='" + centralServer + "/contact-us/'><span>Contact Us</span></a></li>" +
+					"<li class='nav-about'><a href='" + centralServer + "/about/'><span>About the Atlas</span></a>" +
 						"<ul>" +
-							"<li><a href='http://test.ala.org.au/about/people/'><span>Working Together</span></a></li>" +
-							"<li><a href='http://test.ala.org.au/about/portfolio-of-projects/'><span>Projects</span></a></li>" +
-							"<li><a href='http://test.ala.org.au/about/project-time-line/'><span>Project Time Line</span></a></li>" +
-							"<li><a href='http://test.ala.org.au/about/governance/'><span>Governance</span></a></li>" +
-							"<li><a href='http://test.ala.org.au/about/media-centre/'><span>Media Centre</span></a></li>" +
-							"<li><a href='http://test.ala.org.au/about/newsevents/'><span>News &amp; Events</span></a></li>" +
-							"<li><a href='http://test.ala.org.au/about/resources/'><span>Resources</span></a></li>" +
+							"<li><a href='" + centralServer + "/about/people/'><span>Working Together</span></a></li>" +
+							"<li><a href='" + centralServer + "/about/portfolio-of-projects/'><span>Projects</span></a></li>" +
+							"<li><a href='" + centralServer + "/about/project-time-line/'><span>Project Time Line</span></a></li>" +
+							"<li><a href='" + centralServer + "/about/governance/'><span>Governance</span></a></li>" +
+							"<li><a href='" + centralServer + "/about/media-centre/'><span>Media Centre</span></a></li>" +
+							"<li><a href='" + centralServer + "/about/newsevents/'><span>News &amp; Events</span></a></li>" +
+							"<li><a href='" + centralServer + "/about/resources/'><span>Resources</span></a></li>" +
 						"</ul>" +
 					"</li>" +
+					"<li class='nav-myprofile nav-right'><a href='" + casServer + "/cas/login?service=http://test.ala.org.au/wp-login.php?redirect_to=http://test.ala.org.au/my-profile/'><span>My Profile</span></a></li>" +
 					loginLogoutListItem + 
 				"</ul>" +
 				"<!-- WP Menubar 4.7: end menu nav-site, template Superfish, CSS  -->" +
