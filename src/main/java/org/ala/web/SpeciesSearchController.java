335a07389bd4cee5edc0208080bf0468de03351c
djtfmartin <djtfmartin@gmail.com>
2010-10-18 04:06:45 +0000

    javadoc
    git-svn-id: http://ala-bie.googlecode.com/svn/trunk/bie-webapp@2069 de2a7fd8-b20c-11de-9ad3-fd5786690477

--- src/main/java/org/ala/web/SpeciesSearchController.java
+++ src/main/java/org/ala/web/SpeciesSearchController.java
@@ -14,6 +14,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+/**
+ * Simple controller providing quick search mechanisms before
+ * routing off to separate tools.
+ * 
+ * @author Dave Martin
+ */
 @Controller("speciesSearchController")
 public class SpeciesSearchController {
 
