a970bc177cfa51fc900b712ccffd851e28a0bda6
djtfmartin <djtfmartin@gmail.com>
2010-12-24 05:56:07 +0000

    removed import
    git-svn-id: http://ala-bie.googlecode.com/svn/trunk/bie-hbase@2491 de2a7fd8-b20c-11de-9ad3-fd5786690477

--- src/main/java/org/ala/dao/CassandraPelopsHelper.java
+++ src/main/java/org/ala/dao/CassandraPelopsHelper.java
@@ -20,7 +20,6 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import org.ala.model.Ranking;
 
 import org.ala.util.CassandraSubColumnType;
 import org.apache.cassandra.thrift.Column;
