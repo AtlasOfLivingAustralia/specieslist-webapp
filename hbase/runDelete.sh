d3b41a8fe987bf63e0841801807ee0945c7c80da
dcwmk282@gmail.com <dcwmk282@gmail.com>
2011-03-23 05:05:29 +0000

    git-svn-id: http://ala-bie.googlecode.com/svn/branches/ranking@2757 de2a7fd8-b20c-11de-9ad3-fd5786690477

--- hbase/runDelete.sh
+++
@@ -1,7 +0,0 @@
-echo "BULK DELETE : running processing $('date')"
-args=("$@")
-echo "args = $@"
-cd target
-jar xf bie-hbase-assembly.jar lib lib
-export CLASSPATH=bie-hbase-assembly.jar
-java -Xms128m -Xmx512m -classpath $CLASSPATH org.ala.util.CassandraBatchDelete "$@"
