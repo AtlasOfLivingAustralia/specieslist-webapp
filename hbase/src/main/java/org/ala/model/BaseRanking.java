8c20406165e2203b69532b1cd2da9908000ee905
dcwmk282@gmail.com <dcwmk282@gmail.com>
2011-03-23 05:12:44 +0000

    init version
    git-svn-id: http://ala-bie.googlecode.com/svn/branches/ranking@2760 de2a7fd8-b20c-11de-9ad3-fd5786690477

---
+++ hbase/src/main/java/org/ala/model/BaseRanking.java
@@ -0,0 +1,102 @@
+package org.ala.model;
+
+import java.util.Map;
+
+//import org.apache.commons.lang.builder.EqualsBuilder;
+
+public class BaseRanking implements Comparable<BaseRanking> {
+	protected String userIP;
+	protected String userId;
+	protected String fullName;
+	protected boolean positive;
+	protected boolean isBlackListed = false;
+//	protected String uri;
+	
+	protected Map<String, String> compareFieldValue;
+	
+	@Override
+	public int compareTo(BaseRanking o) {
+		return 0;
+	}
+
+//	public String getUri() {
+//		return uri;
+//	}
+//
+//	public void setUri(String uri) {
+//		this.uri = uri;
+//	}
+	
+	public Map<String, String> getCompareFieldValue() {
+		return compareFieldValue;
+	}
+
+	public void setCompareFieldValue(Map<String, String> compareFieldValue) {
+		this.compareFieldValue = compareFieldValue;
+	}
+	
+	/**
+	 * @return the userIP
+	 */
+	public String getUserIP() {
+		return userIP;
+	}
+
+	/**
+	 * @param userIP the userIP to set
+	 */
+	public void setUserIP(String userIP) {
+		this.userIP = userIP;
+	}
+
+	/**
+	 * @return the userId
+	 */
+	public String getUserId() {
+		return userId;
+	}
+
+	/**
+	 * @param userId the userId to set
+	 */
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	/**
+	 * @return the fullName
+	 */
+	public String getFullName() {
+		return fullName;
+	}
+
+	/**
+	 * @param fullName the fullName to set
+	 */
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+	
+	public boolean isBlackListed() {
+		return isBlackListed;
+	}
+
+	public void setBlackListed(boolean isBlackListed) {
+		this.isBlackListed = isBlackListed;
+	}
+
+	/**
+	 * @return the positive
+	 */
+	public boolean isPositive() {
+		return positive;
+	}
+
+	/**
+	 * @param positive the positive to set
+	 */
+	public void setPositive(boolean positive) {
+		this.positive = positive;
+	}
+
+}
