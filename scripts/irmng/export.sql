2564c1d502c9ea26794cded94ce64f178c87421c
natasha.carter@csiro.au <natasha.carter@csiro.au>
2012-01-24 05:55:01 +0000

    new IRMNG import and export scripts
    git-svn-id: http://ala-bie.googlecode.com/svn/trunk/bie-profile@3483 de2a7fd8-b20c-11de-9ad3-fd5786690477

--- scripts/irmng/export.sql
+++ scripts/irmng/export.sql
@@ -1,25 +1,27 @@
--- Export IRMNG data from MySQL DB irmng20091116 on alaslvdb1-cbr.vm.csiro.au
+-- Export IRMNG data from MySQL DB irmng20111014 on diasbtest1-cbr.vm.csiro.au
 --
 -- IRMNG Family extant and habitat data
 
-select family_id, family, kingdom, fam_extant_flag, fam_habitat_flag from MASTER_FAMLIST
-where fam_extant_flag != 'F'
+select FAMILY_ID, FAMILY, KINGDOM, FAM_EXTANT_FLAG, FAM_HABITAT_FLAG, IS_EXTANT, IS_RECENTLY_EXTINCT, IS_FOSSIL, IS_MARINE, IS_BRACKISH, IS_FRESHWATER, IS_TERRESTRIAL 
+from MASTER_FAMLIST
+where FAM_EXTANT_FLAG != 'F'
 order by family
 into outfile '/data/bie-staging/irmng/family_list.txt'
 fields enclosed by '"';
 
 -- IRMNG Genus extant and habitat data
 
-select genus_id, genus, family, gen_extant_flag, gen_habitat_flag from MASTER_GENLIST
-where gen_extant_flag != 'F'
+select GENUS_ID, GENUS, FAMILY, IRMNG_HIERARCHY, GEN_EXTANT_FLAG, GEN_HABITAT_FLAG, IS_EXTANT, IS_RECENTLY_EXTINCT, IS_FOSSIL, IS_MARINE, IS_BRACKISH, IS_FRESHWATER, IS_TERRESTRIAL 
+from MASTER_GENLIST 
+where GEN_EXTANT_FLAG <>'F'
 order by genus
 into outfile '/data/bie-staging/irmng/genus_list.txt'
 fields enclosed by '"';
 
 -- IRMNG Species extant and habitat data
 
-select species_id, concat(g.genus, ' ', species), concat(genus_orig, ' ', species), sp_extant_flag, sp_habitat_flag from MASTER_SPLIST sp, MASTER_GENLIST g
-where sp_extant_flag != 'F' and sp.genus_id=g.genus_id
+select sp.SPECIES_ID, concat(g.GENUS, ' ', sp.SPECIES), g.GENUS, IRMNG_HIERARCHY + '-' + g.FAMILY, sp_extant_flag, sp_habitat_flag from MASTER_SPLIST sp, MASTER_GENLIST g
+where sp.SP_EXTANT_FLAG != 'F' and sp.GENUS_ID=g.GENUS_ID
 order by g.genus, species
 into outfile '/data/bie-staging/irmng/species_list.txt'
 fields enclosed by '"';
