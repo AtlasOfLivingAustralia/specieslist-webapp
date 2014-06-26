ade8fcff84bf7c30ec2a618426ecf36e89c061c9
nickdos <nickdos@gmail.com>
2014-06-26 04:27:01 +0000

    Added grails.project.groupId = "au.org.ala"  to config.groovy.
    git-svn-id: http://ala-bie.googlecode.com/svn/trunk/specieslist-webapp@4398 de2a7fd8-b20c-11de-9ad3-fd5786690477

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -1,6 +1,9 @@
 /******************************************************************************\
  *  CONFIG MANAGEMENT
  \******************************************************************************/
+
+grails.project.groupId = "au.org.ala"
+
 def appName = grails.util.Metadata.current.'app.name'
 def ENV_NAME = "${appName.toUpperCase()}_CONFIG"
 default_config = "/data/${appName}/config/${appName}-config.properties"
@@ -259,3 +262,26 @@ springcache {
         }
     }
 }
+// Uncomment and edit the following lines to start using Grails encoding & escaping improvements
+
+/* remove this line 
+// GSP settings
+grails {
+    views {
+        gsp {
+            encoding = 'UTF-8'
+            htmlcodec = 'xml' // use xml escaping instead of HTML4 escaping
+            codecs {
+                expression = 'html' // escapes values inside null
+                scriptlet = 'none' // escapes output from scriptlets in GSPs
+                taglib = 'none' // escapes output from taglibs
+                staticparts = 'none' // escapes output from static template parts
+            }
+        }
+        // escapes all not-encoded output at final stage of outputting
+        filteringCodecForContentType {
+            //'text/html' = 'html'
+        }
+    }
+}
+remove this line */
