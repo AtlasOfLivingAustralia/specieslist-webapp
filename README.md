788344a8093c6cbaa8b51421f228183f39347ea6
mbohun <martin.bohun@gmail.com>
2014-10-07 17:16:01 +1100

    GENERATED: adding travis-ci.org support

---
+++ README.md
@@ -0,0 +1,1 @@
+### specieslist-webapp   [![Build Status](https://travis-ci.org/AtlasOfLivingAustralia/specieslist-webapp.svg?branch=master)](https://travis-ci.org/AtlasOfLivingAustralia/specieslist-webapp)
--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -80,6 +80,7 @@ grails.project.dependency.resolution = {
     }
 
     plugins {
+        build ":release:3.0.1"
         runtime ":hibernate:3.6.10.15"
         runtime ":jquery:1.7.1"
         runtime ":resources:1.2"
