fcb0e3715a3d2be58a2cfcec466a5d5833531fbb
m-r-c <m-r-c@users.noreply.github.com>
2014-12-16 17:18:11 +1100

    Fixed bug with interpretting the string 'false' as True in an if statement

---
+++ test/unit/au/org/ala/specieslist/service/HelperServiceTest.groovy
@@ -0,0 +1,47 @@
+package au.org.ala.specieslist.service
+
+import au.org.ala.specieslist.HelperService
+import grails.test.mixin.TestFor
+import grails.test.mixin.TestMixin
+import grails.test.mixin.support.GrailsUnitTestMixin
+import spock.lang.Specification
+import spock.lang.Unroll
+
+@TestFor(HelperService)
+@TestMixin(GrailsUnitTestMixin)
+@Unroll
+class HelperServiceTest extends Specification {
+
+    def helperService = new HelperService()
+
+    def setup() {
+        helperService.grailsApplication = grailsApplication
+    }
+
+    def "addDataResourceForList should return a dummy url when collectory.enableSync"() {
+        when:
+        grailsApplication.config.collectory.enableSync = item
+        grailsApplication.config.collectory.baseURL = "http://blabla.com"
+
+        then:
+        String url = helperService.addDataResourceForList([:])
+        assert url.contains("tmp/drt")
+
+        where:
+        item << [false, "false", " ", "", null]
+    }
+
+    def "updateDataResourceForList should return a dummy url when collectory.enableSync"() {
+        when:
+        grailsApplication.config.collectory.enableSync = item
+        grailsApplication.config.collectory.baseURL = "http://blabla.com"
+
+        then:
+        String url = helperService.updateDataResourceForList("id", [:])
+        assert url.contains("tmp/drt")
+
+        where:
+        item << [false, "false", " ", "", null]
+    }
+
+}
