70268280e38926e69e2dca2db1c256761d408b88
m-r-c <m-r-c@users.noreply.github.com>
2014-12-16 17:54:45 +1100

    Fixes #6 "Blank (space) CSV record causes upload to fail silently"

--- test/unit/au/org/ala/specieslist/service/HelperServiceTest.groovy
+++ test/unit/au/org/ala/specieslist/service/HelperServiceTest.groovy
@@ -1,15 +1,22 @@
 package au.org.ala.specieslist.service
 
+import au.org.ala.checklist.lucene.CBIndexSearch
 import au.org.ala.specieslist.HelperService
+import au.org.ala.specieslist.SpeciesList
+import au.org.ala.specieslist.SpeciesListItem
+import au.org.ala.specieslist.SpeciesListKVP
+import grails.test.mixin.Mock
 import grails.test.mixin.TestFor
 import grails.test.mixin.TestMixin
+import grails.test.mixin.domain.DomainClassUnitTestMixin
 import grails.test.mixin.support.GrailsUnitTestMixin
 import spock.lang.Specification
 import spock.lang.Unroll
 
 @TestFor(HelperService)
-@TestMixin(GrailsUnitTestMixin)
+@TestMixin([GrailsUnitTestMixin, DomainClassUnitTestMixin])
 @Unroll
+@Mock([SpeciesListItem, SpeciesList, SpeciesListKVP])
 class HelperServiceTest extends Specification {
 
     def helperService = new HelperService()
@@ -44,4 +51,30 @@ class HelperServiceTest extends Specification {
         item << [false, "false", " ", "", null]
     }
 
+    def "insertSpeciesItem should not create a KVP record for a blank value"() {
+
+        helperService.setCbIdxSearcher(Mock(CBIndexSearch))
+
+        when:
+        def speciesItem = helperService.insertSpeciesItem((String[]) [blank], "Dr1", -1, (String[]) ["Header1"],
+                [:], 0)
+
+        then:
+        assert speciesItem.kvpValues == null || speciesItem.kvpValues.empty
+
+        where:
+        blank << [" ", "\t", "\n", "   ", ""]
+    }
+
+    def "insertSpeciesItem should create a KVP record for a non-blank value"() {
+
+        helperService.setCbIdxSearcher(Mock(CBIndexSearch))
+
+        when:
+        def speciesItem = helperService.insertSpeciesItem((String[]) ["test"], "Dr1", -1, (String[]) ["Header1"],
+                [:], 0)
+
+        then:
+        assert speciesItem.kvpValues.size() == 1
+    }
 }
