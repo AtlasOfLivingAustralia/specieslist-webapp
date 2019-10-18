090133e8b1e61a0d77018f612f7a1ed246e18ac7
vjrj <vjrj@ourproject.org>
2019-10-18 11:25:07 +0200

    Usage of external ala-i18n resources

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,3 +1,13 @@
 // Place your Spring DSL code here
 beans = {
+    // Custom message source
+    messageSource(org.springframework.context.support.ReloadableResourceBundleMessageSource) {
+        basenames = [
+                "file:///var/opt/atlas/i18n/specieslist-webapp/messages",
+                "file:///opt/atlas/i18n/specieslist-webapp/messages",
+                "WEB-INF/grails-app/i18n/messages"
+        ] as String[]
+        cacheSeconds = (60 * 60 * 6) // 6 hours
+        useCodeAsDefaultMessage = false
+    }
 }
