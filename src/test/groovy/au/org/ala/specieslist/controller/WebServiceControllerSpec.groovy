0ab63bed00fb275db3d3fc704f19fbff7bb6adfa
Rita <Rita.Chen@csiro.au>
2021-10-25 15:14:18 +1100

    #189 grails 4, fixed unit test that caused build to fail.

--- src/test/groovy/au/org/ala/specieslist/controller/WebServiceControllerSpec.groovy
+++ src/test/groovy/au/org/ala/specieslist/controller/WebServiceControllerSpec.groovy
@@ -1,6 +1,6 @@
 package au.org.ala.specieslist.controller
 
-import au.org.ala.specieslist.QueryService
+
 import au.org.ala.specieslist.SpeciesList
 import au.org.ala.specieslist.SpeciesListItem
 import au.org.ala.specieslist.SpeciesListKVP
@@ -105,76 +105,5 @@ class WebServiceControllerSpec extends Specification implements ControllerUnitTe
         then:
         assert response.status == HttpStatus.SC_OK
         assert response.text ==  "[\"key 1\"]"
-    }
-
-    void "test SpeciesLists"() {
-        setup:
-//        controller.queryService = Mock(QueryService)
-
-        def list1 = new SpeciesList(dateCreated: new Date(), lastUpdated: new Date(), dataResourceUid: "1",
-                username: "b", description: "1", firstName: "first user", listName: "list a one", surname: "xyz")
-        def list2 = new SpeciesList(dateCreated: new Date(), lastUpdated: new Date(), dataResourceUid: "2",
-                username: "c", description: "2", firstName: "first user", listName: "list b two", surname: "surname")
-        def list3 = new SpeciesList(dateCreated: new Date(), lastUpdated: new Date(), dataResourceUid: "3",
-                username: "a", description: "3", firstName: "second user", listName: "list c three", surname: "xyz")
-        list1.save(flush: true, failOnError: true)
-        list2.save(flush: true, failOnError: true)
-        list3.save(flush: true, failOnError: true)
-
-        when:
-        controller.queryService = Mock(QueryService)
-        controller.getListDetails()
-
-        then:
-        assert controller.response.json.lists.size() == 3
-    }
-
-    void "test SpeciesLists user order, q filtering"() {
-        setup:
-        controller.queryService = Mock(QueryService)
-
-        def list1 = new SpeciesList(dateCreated: new Date(), lastUpdated: new Date(), dataResourceUid: "1",
-                username: "b", description: "1", firstName: "first user", listName: "list a one", surname: "xyz")
-        def list2 = new SpeciesList(dateCreated: new Date(), lastUpdated: new Date(), dataResourceUid: "2",
-                username: "c", description: "2", firstName: "first user", listName: "list b two", surname: "surname")
-        def list3 = new SpeciesList(dateCreated: new Date(), lastUpdated: new Date(), dataResourceUid: "3",
-                username: "a", description: "3", firstName: "second user", listName: "list c three", surname: "xyz")
-        list1.save(flush: true, failOnError: true)
-        list2.save(flush: true, failOnError: true)
-        list3.save(flush: true, failOnError: true)
-
-        when:
-        controller.params.user = 'a'
-        controller.params.q = 'xyz'
-        controller.params.sort = 'userId'
-        controller.getListDetails()
-
-        then:
-        assert controller.response.json.lists.size() == 2
-        assert controller.response.json.lists[0].dataResourceUid == '3'
-        assert controller.response.json.lists[1].dataResourceUid == '1'
-    }
-
-    void "test SpeciesLists default order, q filter"() {
-        setup:
-        controller.queryService = Mock(QueryService)
-        def list1 = new SpeciesList(dateCreated: new Date(), lastUpdated: new Date(), dataResourceUid: "1",
-                username: "b", description: "1", firstName: "first user", listName: "list a one", surname: "xyz")
-        def list2 = new SpeciesList(dateCreated: new Date(), lastUpdated: new Date(), dataResourceUid: "2",
-                username: "c", description: "2", firstName: "first user", listName: "list b two", surname: "surname")
-        def list3 = new SpeciesList(dateCreated: new Date(), lastUpdated: new Date(), dataResourceUid: "3",
-                username: "a", description: "3", firstName: "second user", listName: "list c three", surname: "xyz")
-        list1.save(flush: true, failOnError: true)
-        list2.save(flush: true, failOnError: true)
-        list3.save(flush: true, failOnError: true)
-
-        when:
-        controller.params.q = 'first'
-        controller.getListDetails()
-
-        then:
-        assert controller.response.json.lists.size() == 2
-        assert controller.response.json.lists[0].dataResourceUid == '1'
-        assert controller.response.json.lists[1].dataResourceUid == '2'
     }
 }
